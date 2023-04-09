09f345028238bf183585aae647f641029d944a6b
giinnynguyen <tieuphung2205@gmail.com>
2023-04-09 21:40:21 +0700

    Deploy website - based on d0b2f6e2e1bf0f77f13dfb59c734b584c588f367

--- src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import  { Redirect } from 'react-router-dom';
-
-export default function Home(): JSX.Element {
-  return <Redirect to='/docs/intro' />;
-}
---
+++ vi/assets/js/692f91d2.0d86df20.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkginnywiki=self.webpackChunkginnywiki||[]).push([[157],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
